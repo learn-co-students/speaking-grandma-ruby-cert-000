e8bb7574da40325284b15c760384ab85c97861d8
Karl Murray <karlmurray1138@gmail.com>
2016-09-22 16:16:01 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,9 @@
-value2 = gets.chomp
-
 def speak_to_grandma(value1)
-
   if value1 == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-
   elsif value1 == value1.upcase
     puts "NO, NOT SINCE 1938!"
-
   else
     puts "HUH?! SPEAK UP, SONNY!"
-
 end
 end
-speak_to_grandma(value2)
